ec092f919bbbc38bf91d7dcb12199322a01e6c3d
Rodney Stead <rodcstead@gmail.com>
2024-06-20 00:29:46 -0230

    made init app to check if the app mhas been initialized

--- PO.js
+++ PO.js
@@ -1,6 +1,7 @@
 global.DEBUG = false;
 
 const fs = require("fs");
+const { checkAppInit } = require("./init.js");
 
 const myArgs = process.argv.slice(2);
 
@@ -9,6 +10,8 @@ if (DEBUG && myArgs.length >= 1) console.log("My Args: " + myArgs);
 switch (myArgs[0]) {
   case "init":
   case "i":
+    if (DEBUG) console.log(myArgs[0], '- checking app initialization....');
+    checkAppInit();
     break;
   case "config":
   case "c":
@@ -20,3 +23,4 @@ switch (myArgs[0]) {
   case "--h":
   default:
 }
+
---
+++ init.js
@@ -0,0 +1,18 @@
+const myArgs = process.argv.slice(2);
+const fs = require('fs');
+function checkAppInit() {
+    switch (myArgs[0]) {
+        case "init":
+        case "i":
+          if (fs.existsSync('./PO.js')) {
+            console.log('Init has been run previously');
+          } else {
+            console.log('Init has not been run yet');
+          }
+          break;
+      }
+};
+
+module.exports = {
+    checkAppInit
+};
