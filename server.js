1486f63fd17522ed24a679eefdf410567a992ccf
unknown <you@example.com>
2024-06-23 19:48:26 -0230

    Add functionality to update config file

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const fs = require('fs');
+const { configjson } = require('./templates.js');
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+// Route to view the config file
+app.get('/config', (req, res) => {
+  res.json(configjson);
+});
+
+// Route to update the config file
+app.post('/config', (req, res) => {
+  const newConfig = req.body;
+  // Update the config file with new data
+  Object.assign(configjson, newConfig);
+  fs.writeFileSync('./templates.js', `const configjson = ${JSON.stringify(configjson, null, 2)};\n\nmodule.exports = { configjson };`);
+  res.json({ message: 'Config updated', config: configjson });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
